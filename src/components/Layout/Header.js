7ad53b758a6b3be37201dffa4d3d446f7d60800d
Anastasia Haller <anadana87@gmail.com>
2023-07-31 11:10:13 +0200

    added HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,12 @@
 import sushiImage from '../../assets/sushi.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return <>
             <header className={styles.header}>
                 <h1>Japona Kitchen</h1>
-                <button>Cart</button>
+                <HeaderCartButton>Cart</HeaderCartButton>
             </header>
             <div className={styles['main-image']}>
                 <img src={sushiImage} alt="sushi"/>
