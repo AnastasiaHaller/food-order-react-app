c3c50a334c73509803b881c89f8f2217a00c1e97
Anastasia Haller <anadana87@gmail.com>
2023-08-23 16:36:13 +0200

    added + and - functionality in Cart

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -7,7 +7,7 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-    if(action.type === 'ADD_ITEM') {
+    if (action.type === 'ADD_ITEM') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
         const existingCartItemIndex = state.items.findIndex((item) => {
@@ -39,6 +39,35 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount,
         };
     }
+
+    if (action.type === 'REMOVE_ITEM') {
+
+        const existingCartItemIndex = state.items.findIndex((item) => {
+            return item.id ===action.id;
+        });
+
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+        let updatedItems;
+
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter((item) => {
+                item.id != action.id
+            })
+        } else {
+            const updatedItem ={...existingCartItem, amount: existingCartItem.amoung - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        }
+    }
+
     return defaultCartState;
 };
 
