c37e746857ba511669c46e9a7bd83c0ae4d47ccc
Anastasia Haller <anadana87@gmail.com>
2023-08-24 14:12:51 +0200

    added animation for Cart Button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,11 @@
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css'
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-contex';
 
 const HeaderCartButton = (props) => {
+
+    const [isButtonAnimated, setIsButtonAnimated] = useState(false);
 
     const cartContext = useContext(CartContext);
 
@@ -11,8 +13,26 @@ const HeaderCartButton = (props) => {
         return currentValue + item.amount;
     }, 0);
 
+    const buttonClasses = `${styles.button} ${isButtonAnimated ? styles.bump : ''}`;
+
+    useEffect(() => {
+        if (cartContext.items.length === 0) {
+            return;
+        }
+        setIsButtonAnimated(true);
+
+        const timer = setTimeout(() => {
+            setIsButtonAnimated(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [cartContext.items]);
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={buttonClasses} onClick={props.onClick}>
             <span className={styles.icon}><CartIcon /></span>
             <span>Cart</span>
             <span className={styles.badge}>{cartItemsNumber}</span>
