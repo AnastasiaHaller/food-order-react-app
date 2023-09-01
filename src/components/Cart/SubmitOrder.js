322c2d17b15a5cc22df9d55af4f0e2f1c5318bca
Anastasia Haller <anadana87@gmail.com>
2023-09-01 11:27:57 +0200

    get input from form
    use useRef to get input values

--- src/components/Cart/SubmitOrder.js
+++ src/components/Cart/SubmitOrder.js
@@ -1,24 +1,33 @@
+import { useRef } from 'react';
 import styles from './SubmitOrder.module.css';
 
 const SubmitOrder = (props) => {
 
+    const nameInputRef = useRef();
+    const cityInputRef = useRef();
+    const addressInputRef = useRef();
+
     const confirmOrderHandler = (event) => {
         event.preventDefault();
+
+        const enteredName = nameInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
     };
 
     return (
         <form onSubmit={confirmOrderHandler}>
             <div className={styles.control}>
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name"/>
+                <input type="text" id="name" ref={nameInputRef} />
             </div>
             <div className={styles.control}>
                 <label htmlFor="city">City</label>
-                <input type="text" id="city"/>
+                <input type="text" id="city" ref={cityInputRef} />
             </div>
             <div className={styles.control}>
                 <label htmlFor="address">Address</label>
-                <input type="text" id="address"/>
+                <input type="text" id="address" ref={addressInputRef} />
             </div>
             <div className={styles.actions}>
                 <button className={styles.submit}>Confirm Order</button>
