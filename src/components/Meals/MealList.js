8619c9603de05092356262715b3565c595577299
Anastasia Haller <anadana87@gmail.com>
2023-09-01 10:42:39 +0200

    add loading meals from server
    http get request to get the meals list

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -1,58 +1,93 @@
+import { useEffect, useState } from 'react';
 import styles from './MealList.module.css';
 import Card from "../UI/Card";
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-      id: "m1",
-      name: 'Maki "Naomi"',
-      description:
-        "Philadelphia, chicken fillet, masago, tomato, cucumber, sesame",
-      price: 11.99,
-    },
-    {
-      id: "m2",
-      name: "Spice in salmon",
-      description: "Rice, salmon, spice sauce",
-      price: 3.99,
-    },
-    {
-      id: "m3",
-      name: "Maki with eel",
-      description: "Smoked eel, unagi sauce, sesame",
-      price: 4.99,
-    },
-    {
-      id: "m4",
-      name: "Salad with salmon",
-      description:
-        "Rice, salmon, cucumber, nori, tuna chips, nut sauce",
-      price: 7.99,
-    },
-  ];
+// const DUMMY_MEALS = [
+//     {
+//       id: "m1",
+//       name: 'Maki "Naomi"',
+//       description: "Philadelphia, chicken fillet, masago, tomato, cucumber, sesame",
+//       price: 11.99,
+//     },
+//     {
+//       id: "m2",
+//       name: "Spice in salmon",
+//       description: "Rice, salmon, spice sauce",
+//       price: 3.99,
+//     },
+//     {
+//       id: "m3",
+//       name: "Maki with eel",
+//       description: "Smoked eel, unagi sauce, sesame",
+//       price: 4.99,
+//     },
+//     {
+//       id: "m4",
+//       name: "Salad with salmon",
+//       description:
+//         "Rice, salmon, cucumber, nori, tuna chips, nut sauce",
+//       price: 7.99,
+//     },
+//   ];
 
 
 const MealList = () => {
 
-    const mealList = DUMMY_MEALS.map((meal) => (
-      <MealItem
-        key={meal.id}
-        id={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
-    ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch("https://joke-fcdf0-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
     return (
-        <section className={styles.meals}>
-          <Card>
-            <ul>
-                {mealList}
-            </ul>
-          </Card>
-        </section>
+      <section className={styles.loading}>
+        <p>Loading...</p>
+      </section>
     );
+  }
+
+  const mealList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+      <section className={styles.meals}>
+        <Card>
+          <ul>
+              {mealList}
+          </ul>
+        </Card>
+      </section>
+  );
 
 };
 
